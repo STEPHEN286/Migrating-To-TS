5d5e720cad38529058c94082ed819f6161193f70
STEPHEN286 <kwofie286@gmail.com>
2025-04-23 00:22:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+// variables in ts
+let message = "Hello, TypeScript!";
+let count = 42;
+let isActive = true;
+// console.log(message); // Output: Hello, TypeScript!
+// how to create a func\tion in typescript
+// function greet(name: string): string {
+//     return `Hello, ${name}!`;
+//   }
+//   console.log(greet("World"));
+function sumOfTwoNumbers(a, b) {
+    return a + b;
+}
+console.log(sumOfTwoNumbers(5, 10)); // Output: 15
---
+++ index.ts
@@ -0,0 +1,26 @@
+
+
+
+// variables in ts
+let message: string = "Hello, TypeScript!";
+let count: number = 42; 
+let isActive: boolean = true;
+
+
+
+// console.log(message); // Output: Hello, TypeScript!
+// how to create a func\tion in typescript
+// function greet(name: string): string {
+//     return `Hello, ${name}!`;
+//   }
+  
+//   console.log(greet("World"));
+
+
+function sumOfTwoNumbers(a: number, b: number): number {
+    return a + b;
+  }
+
+  console.log(sumOfTwoNumbers(5, 10)); // Output: 15
+
+  
