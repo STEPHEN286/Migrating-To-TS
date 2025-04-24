2108f6875c46bf6d0cfed6235bf662999f42edc0
STEPHEN286 <kwofie286@gmail.com>
2025-04-24 00:12:51 +0000

    arrays and tuples

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 "use strict";
 // variables in ts
-let message = "Hello, TypeScript!";
-let count = 42;
-let isActive = true;
+// let message: string = "Hello, TypeScript!";
+// let count: number = 42; 
+// let isActive: boolean = true;
 // console.log(message); // Output: Hello, TypeScript!
 // how to create a func\tion in typescript
 // function greet(name: string): string {
 //     return `Hello, ${name}!`;
 //   }
 //   console.log(greet("World"));
-function sumOfTwoNumbers(a, b) {
-    return a + b;
-}
-console.log(sumOfTwoNumbers(5, 10)); // Output: 15
+// function sumOfTwoNumbers(a: number, b: number): number {
+//     return a + b;
+//   }
+// console.log(sumOfTwoNumbers(5, 10)); // Output: 15
+let favoriteColors = ["gold", "gray", "blue"]; //array 
+// let myProfile: [string, number] = ["stephen", 2];
+console.log(favoriteColors[1]);
--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@
 
 
 // variables in ts
-let message: string = "Hello, TypeScript!";
-let count: number = 42; 
-let isActive: boolean = true;
+// let message: string = "Hello, TypeScript!";
+// let count: number = 42; 
+// let isActive: boolean = true;
 
 
 
@@ -17,10 +17,15 @@ let isActive: boolean = true;
 //   console.log(greet("World"));
 
 
-function sumOfTwoNumbers(a: number, b: number): number {
-    return a + b;
-  }
+// function sumOfTwoNumbers(a: number, b: number): number {
+//     return a + b;
+//   }
 
-  console.log(sumOfTwoNumbers(5, 10)); // Output: 15
+  // console.log(sumOfTwoNumbers(5, 10)); // Output: 15
+
+  let favoriteColors: string[] = ["gold", "gray", "blue"]; //array 
+  // let myProfile: [string, number] = ["stephen", 2]; tuple
+
+  console.log(favoriteColors[1])
 
   
